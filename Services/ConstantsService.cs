17611349b594cb8e875625b24b55e450d1aefa27
realgarit <github@realgar.ch>
2025-11-27 23:54:59 +0100

    version bump

--- Services/ConstantsService.cs
+++ Services/ConstantsService.cs
@@ -19,7 +19,7 @@ namespace teams_phonemanager.Services
 
         public static class Application
         {
-            public const string Version = "Version 3.4.2";
+            public const string Version = "Version 3.4.3";
             public const string Copyright = "Realgar © 2025. MIT License.";
         }
 
