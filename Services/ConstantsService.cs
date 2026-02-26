fdb05b5cd1f2d1329ac11f5b284eb828a243db53
realgarit <github@realgar.ch>
2026-02-26 23:22:13 +0100

    chore: Update version to 3.10.0 in project files and manifest

--- Services/ConstantsService.cs
+++ Services/ConstantsService.cs
@@ -19,7 +19,7 @@ namespace teams_phonemanager.Services
 
         public static class Application
         {
-            public const string Version = "Version 3.9.0";
+            public const string Version = "Version 3.10.0";
             public const string Copyright = "Realgar © 2026. MIT License.";
         }
 
